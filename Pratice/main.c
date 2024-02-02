c8f6b9f6063750f7fd1600848d01cac5dabd73f0
Gautamvaishnav-git <gauravvaishnav8690@gmail.com>
2024-02-02 22:39:04 +0530

    pratice questions

---
+++ Pratice/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+// function for questions
+// void calculatePerimeter();
+// void printCube();
+// void printAvgOfThree();
+// void isDigit();
+// void findSmallest();
+
+int main()
+{
+    // calculatePerimeter();
+    // printCube();
+    // printAvgOfThree();
+    // isDigit();
+    // findSmallest();
+    return 0;
+}
+
+// Q1. Write a program to calculate perimeter of rectangle.
+// Take sides, a & b, from the user.
+// void calculatePerimeter()
+// {
+//     float width, length;
+//     printf("Enter width of rectangle: ");
+//     scanf("%f", &width);
+//     printf("Enter length of rectangle: ");
+//     scanf("%f", &length);
+//     printf("Perimeter of rectangle is %f", width * length);
+// }
+
+// Q2 Take a number(n) from user & output its cube(n*n*n)
+// void printCube()
+// {
+//     int n;
+//     printf("Enter n ");
+//     scanf("%d", &n);
+//     printf("Perimeter of rectangle is %d", n * n * n);
+// }
+
+// Q3. Write a program to print the average of 3 numbers.
+// void printAvgOfThree()
+// {
+//     int a, b, c;
+//     printf("Enter a: ");
+//     scanf("%d", &a);
+//     printf("Enter b: ");
+//     scanf("%d", &b);
+//     printf("Enter c: ");
+//     scanf("%d", &c);
+//     printf("Perimeter of rectangle is %d", (a + b + c) / 3);
+// }
+
+// Q4. Write a program to check if given character is digit
+// or not.
+// void isDigit()
+// {
+//     char inputChar;
+//     printf("Enter a character: ");
+//     scanf("%c", &inputChar);
+//     if (inputChar >= '0' && inputChar <= '9')
+//     {
+//         printf("%c is a digit\n", inputChar);
+//     }
+//     else
+//     {
+//         printf("%c is not a digit\n", inputChar);
+//     }
+// }
+
+// Q5. Write a program to print the smallest number of two
+// void findSmallest()
+// {
+//     int a, b;
+//     printf("enter first number: ", a);
+//     scanf("%d", &a);
+//     printf("enter second number: ", b);
+//     scanf("%d", &b);
+//     if (a < b)
+//     {
+//         printf("%d is smallest", a);
+//     }
+//     else
+//     {
+//         printf("%d is smallest", b);
+//     }
+// }
+
+
+
+
+
